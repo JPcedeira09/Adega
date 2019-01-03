30f22653503757d107fc85033a3259a1ec99e487
Joao Paulo <joao.paulo@hvarconsulting.com.br>
2019-01-03 16:56:39 -0200

    Primeiro Commit
    Comit Inicial

---
+++ Adega/Cardapio/Cardapio Cell/CardapioTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CardapioTableViewCell.swift
+//  Adega
+//
+//  Created by Joao Paulo on 02/01/19.
+//  Copyright © 2019 Joao Paulo. All rights reserved.
+//
+
+import UIKit
+
+class CardapioTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nome_produto: UILabel!
+    @IBOutlet weak var descricao: UITextView!
+    @IBOutlet weak var valor: UILabel!
+    @IBOutlet weak var imagem: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
---
+++ Adega/Meu Cardapio/Meu Cardapio Cell/old/MeuCardapioTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MeuCardapioTableViewCell.swift
+//  Adega
+//
+//  Created by Joao Paulo on 02/01/19.
+//  Copyright © 2019 Joao Paulo. All rights reserved.
+//
+
+import UIKit
+
+class MeuCardapioTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nome_produto: UILabel!
+    @IBOutlet weak var disponibilidade: UILabel!
+    @IBOutlet weak var imagem_produto: UIImageView!
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
