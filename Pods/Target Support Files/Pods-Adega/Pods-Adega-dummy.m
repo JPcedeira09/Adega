e04e4453c07269faf56f7c9c36870ffc5bb02c75
Joao Paulo <joao.paulo@hvarconsulting.com.br>
2019-01-04 12:02:40 -0200

    Pods Instalados
    Pods instalados para começar a integração via Firebase

---
+++ Pods/Target Support Files/Pods-Adega/Pods-Adega-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Adega : NSObject
+@end
+@implementation PodsDummy_Pods_Adega
+@end
---
+++ Pods/Target Support Files/Pods-AdegaTests/Pods-AdegaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AdegaTests : NSObject
+@end
+@implementation PodsDummy_Pods_AdegaTests
+@end
